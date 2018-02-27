e8eda3054b6198031623bff7a81394673bd0e9e9
dariashifrina <dshifrina@stuy.edu>
2018-02-27 11:05:08 -0500

    woo

--- app.py
+++ app.py
@@ -19,39 +19,45 @@ def main():
 
 @app.route("/respond", methods = ['GET', 'POST'])
 def respond():
-    if request.method=="GET":
-        actor = request.form['actor']
-        director = request.form['director']
-        title = request.form['title']
-        year = request.form['year']
-        genre = request.form['genre']
-        query = request.form['query']
-        if (query == 1):
-            ret = []
-            ret = get_movies(year)
-            return render_template('respond.html', result = ret)
-        elif (query == 2):
-            ret = []
-            ret = get_starred_films(actor)
-            return render_template('respond.html', result = ret)
-        elif (query == 3):
-            ret = []
-            ret = get_director_films(director)
-            return render_template('respond.html', result = ret)
-        elif (query == 4):
-            ret = []
-            ret = genre_year(genre, year)
-            return render_template('respond.html', result = ret)
-        elif (query == 5):
-            ret = []
-            ret = movies_past_year(year)
-            return render_template('respond.html', result = ret)
-        else:
-            ret = []
-            ret = movie_info(title)
-            return render_template('respond.html', result = ret)
+    #if request.method=="POST":
+    print "ya"
+    query = float(request.args['query'])
+    print query
+    if (query == 1):
+        year = request.args['year']
+        ret = []
+        ret = get_movies(year)
+        return render_template('respond.html', result = ret)
+    elif (query == 2):
+        actor = request.args['actor']
+        ret = []
+        ret = get_starred_films(actor)
+        return render_template('respond.html', result = ret)
+    elif (query == 3):
+        director = request.args['director']
+        ret = []
+        ret = get_director_films(director)
+        return render_template('respond.html', result = ret)
+    elif (query == 4):
+        year = request.args['year']
+        genre = request.args['genre']
+        ret = []
+        ret = genre_year(genre, year)
+        return render_template('respond.html', result = ret)
+    elif (query == 5):
+        year = request.args['year']
+        ret = []
+        ret = movies_past_year(year)
+        return render_template('respond.html', result = ret)
+    elif(query ==6):
+        title = request.args['title']
+        print "movie title"
+        ret = []
+        ret = movie_info(title)
+        return render_template('respond.html', result = ret)
     else:
         print "not get"
+        return render_template('index.html')
 
 
 
